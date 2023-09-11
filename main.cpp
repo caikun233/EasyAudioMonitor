26322e483e466f70cb0ef11ce319c419183c3995
caikun233 <69567735+caikun233@users.noreply.github.com>
2023-09-12 01:13:23 +0800

    WHY NO VOICE OUT?????

--- main.cpp
+++ main.cpp
@@ -6,26 +6,17 @@
 
 const int BUFFER_SIZE = 4096;
 
-// 回调函数，用于处理音频数据
 void CALLBACK waveInProc(HWAVEIN hwi, UINT uMsg, DWORD_PTR dwInstance, DWORD_PTR dwParam1, DWORD_PTR dwParam2) {
     if (uMsg == WIM_DATA) {
-        WAVEHDR* waveHdr = (WAVEHDR*)dwParam1;
         HWAVEOUT hWaveOut = (HWAVEOUT)dwInstance;
-
-        // 将音频数据写入输出设备
+        WAVEHDR* waveHdr = (WAVEHDR*)dwParam1;
         waveOutWrite(hWaveOut, waveHdr, sizeof(WAVEHDR));
-        
-        // 重新准备音频输入缓冲区
         waveInAddBuffer(hwi, waveHdr, sizeof(WAVEHDR));
     }
 }
 
 int main() {
-    HWAVEIN hWaveIn;
-    HWAVEOUT hWaveOut;
     WAVEFORMATEX wfx;
-
-    // 音频格式设置
     wfx.wFormatTag = WAVE_FORMAT_PCM;
     wfx.nChannels = 2;
     wfx.nSamplesPerSec = 44100;
@@ -34,7 +25,9 @@ int main() {
     wfx.nAvgBytesPerSec = wfx.nSamplesPerSec * wfx.nBlockAlign;
     wfx.cbSize = 0;
 
-    // 打开音频输入和输出设备
+    HWAVEIN hWaveIn;
+    HWAVEOUT hWaveOut;
+
     if (waveInOpen(&hWaveIn, WAVE_MAPPER, &wfx, (DWORD_PTR)waveInProc, (DWORD_PTR)&hWaveOut, CALLBACK_FUNCTION) != MMSYSERR_NOERROR) {
         std::cerr << "Failed to open audio input device." << std::endl;
         return 1;
@@ -46,22 +39,20 @@ int main() {
         return 1;
     }
 
-    // 音频缓冲区设置
     WAVEHDR waveHdr;
-    waveHdr.lpData = (LPSTR)malloc(BUFFER_SIZE);
+    char buffer[BUFFER_SIZE];
+    waveHdr.lpData = buffer;
     waveHdr.dwBufferLength = BUFFER_SIZE;
     waveHdr.dwFlags = 0;
 
     waveInPrepareHeader(hWaveIn, &waveHdr, sizeof(WAVEHDR));
     waveInAddBuffer(hWaveIn, &waveHdr, sizeof(WAVEHDR));
 
-    // 开始音频捕获和播放
     waveInStart(hWaveIn);
 
     std::cout << "Listening to audio (Press Enter to stop)..." << std::endl;
     std::cin.get();
 
-    // 停止并清理
     waveInStop(hWaveIn);
     waveInReset(hWaveIn);
     waveOutReset(hWaveOut);
@@ -69,7 +60,6 @@ int main() {
     waveInUnprepareHeader(hWaveIn, &waveHdr, sizeof(WAVEHDR));
     waveInClose(hWaveIn);
     waveOutClose(hWaveOut);
-    free(waveHdr.lpData);
 
     return 0;
 }
