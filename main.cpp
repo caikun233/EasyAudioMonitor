6b68b080c53dc214a84047304cf612471ea020de
caikun233 <69567735+caikun233@users.noreply.github.com>
2023-09-12 01:24:26 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,81 +5,68 @@
 
 #pragma comment(lib, "winmm.lib")
 
-// 音频缓冲区大小
 const int BUFFER_SIZE = 4096;
+const int NUM_BUFFERS = 2;
 
-void checkMMResult(MMRESULT result, const char* errorMsg) {
-    if (result != MMSYSERR_NOERROR) {
-        std::cerr << errorMsg << " Error code: " << result << std::endl;
+void CALLBACK waveOutProc(HWAVEOUT hwo, UINT uMsg, DWORD_PTR dwInstance, DWORD_PTR dwParam1, DWORD_PTR dwParam2) {
+    if (uMsg == WOM_DONE) {
+        WAVEHDR* waveHdr = (WAVEHDR*)dwParam1;
+        waveOutWrite(hwo, waveHdr, sizeof(WAVEHDR));
     }
 }
 
 int main() {
-    // 打开默认的音频输入设备（麦克风）
     HWAVEIN hWaveIn;
+    HWAVEOUT hWaveOut;
     WAVEFORMATEX wfx;
+    WAVEHDR waveHdr[NUM_BUFFERS];
+
     wfx.wFormatTag = WAVE_FORMAT_PCM;
-    wfx.nChannels = 2;                // 立体声输入
-    wfx.nSamplesPerSec = 44100;       // 采样率
-    wfx.wBitsPerSample = 16;          // 16位每样本
+    wfx.nChannels = 2;
+    wfx.nSamplesPerSec = 44100;
+    wfx.wBitsPerSample = 16;
     wfx.nBlockAlign = (wfx.nChannels * wfx.wBitsPerSample) / 8;
     wfx.nAvgBytesPerSec = wfx.nSamplesPerSec * wfx.nBlockAlign;
     wfx.cbSize = 0;
 
-    MMRESULT result = waveInOpen(&hWaveIn, WAVE_MAPPER, &wfx, 0, 0, WAVE_FORMAT_DIRECT);
-    checkMMResult(result, "Failed to open audio input device.");
+    if (waveInOpen(&hWaveIn, WAVE_MAPPER, &wfx, 0, 0, WAVE_FORMAT_DIRECT) != MMSYSERR_NOERROR) {
+        std::cerr << "Failed to open audio input device." << std::endl;
+        return 1;
+    }
 
-    // 打开默认的音频输出设备（扬声器）
-    HWAVEOUT hWaveOut;
-    result = waveOutOpen(&hWaveOut, WAVE_MAPPER, &wfx, 0, 0, WAVE_FORMAT_DIRECT);
-    checkMMResult(result, "Failed to open audio output device.");
+    if (waveOutOpen(&hWaveOut, WAVE_MAPPER, &wfx, (DWORD_PTR)waveOutProc, 0, CALLBACK_FUNCTION) != MMSYSERR_NOERROR) {
+        std::cerr << "Failed to open audio output device." << std::endl;
+        waveInClose(hWaveIn);
+        return 1;
+    }
 
-    // 初始化音频输入缓冲区
-    WAVEHDR waveHdr;
-    waveHdr.lpData = (LPSTR)malloc(BUFFER_SIZE);
-    waveHdr.dwBufferLength = BUFFER_SIZE;
-    waveHdr.dwBytesRecorded = 0;
-    waveHdr.dwUser = 0;
-    waveHdr.dwFlags = 0;
-    waveHdr.dwLoops = 0;
+    for (int i = 0; i < NUM_BUFFERS; ++i) {
+        waveHdr[i].lpData = (LPSTR)malloc(BUFFER_SIZE);
+        waveHdr[i].dwBufferLength = BUFFER_SIZE;
+        waveHdr[i].dwBytesRecorded = 0;
+        waveHdr[i].dwUser = 0;
+        waveHdr[i].dwFlags = 0;
+        waveHdr[i].dwLoops = 0;
+        waveInPrepareHeader(hWaveIn, &waveHdr[i], sizeof(WAVEHDR));
+        waveInAddBuffer(hWaveIn, &waveHdr[i], sizeof(WAVEHDR));
+    }
 
-    result = waveInPrepareHeader(hWaveIn, &waveHdr, sizeof(WAVEHDR));
-    checkMMResult(result, "Failed to prepare header for audio input device.");
-
-    result = waveInAddBuffer(hWaveIn, &waveHdr, sizeof(WAVEHDR));
-    checkMMResult(result, "Failed to add buffer to audio input device.");
-
-    // 开始音频输入和输出
-    result = waveInStart(hWaveIn);
-    checkMMResult(result, "Failed to start audio input.");
-
-    result = waveOutWrite(hWaveOut, &waveHdr, sizeof(WAVEHDR));
-    checkMMResult(result, "Failed to write audio output.");
+    waveInStart(hWaveIn);
 
     std::cout << "Listening to audio (Press Enter to stop)..." << std::endl;
     std::cin.get();
 
-    // 停止音频输入和输出
-    result = waveInStop(hWaveIn);
-    checkMMResult(result, "Failed to stop audio input.");
+    waveInStop(hWaveIn);
+    waveInReset(hWaveIn);
+    waveOutReset(hWaveOut);
 
-    result = waveInReset(hWaveIn);
-    checkMMResult(result, "Failed to reset audio input.");
+    for (int i = 0; i < NUM_BUFFERS; ++i) {
+        waveInUnprepareHeader(hWaveIn, &waveHdr[i], sizeof(WAVEHDR));
+        free(waveHdr[i].lpData);
+    }
 
-    result = waveOutReset(hWaveOut);
-    checkMMResult(result, "Failed to reset audio output.");
-
-    // 关闭设备并释放资源
-    result = waveInUnprepareHeader(hWaveIn, &waveHdr, sizeof(WAVEHDR));
-    checkMMResult(result, "Failed to unprepare header for audio input device.");
-
-    result = waveInClose(hWaveIn);
-    checkMMResult(result, "Failed to close audio input device.");
-
-    result = waveOutClose(hWaveOut);
-    checkMMResult(result, "Failed to close audio output device.");
-
-    free(waveHdr.lpData);
+    waveInClose(hWaveIn);
+    waveOutClose(hWaveOut);
 
     return 0;
 }
